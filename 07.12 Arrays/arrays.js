f58e54875b5c424d6b666cbc6c227772eb71996d
Iro_O <zhdanovairyna@gmail.com>
2017-12-08 14:58:55 +0300

    Add files via upload

---
+++ 07.12 Arrays/arrays.js
@@ -0,0 +1,48 @@
+// TASK 1
+
+var list = [1, 2, 3, 7, 5, 6, 7, 8, 9];
+
+list[3] = 4;
+
+console.log(list);
+
+// TASK 2
+
+list.push(10);
+
+console.log(list);
+
+// TASK 3
+
+var values = ["John Doe", 3, true];
+
+// TASK 4
+
+var bool = values.pop();
+
+console.log(bool);
+
+console.log(values);
+
+//TASK 5
+
+var movie1 = [16, "Candles"];
+
+var movie2 = [3, "Men", "and", "a", "Baby"];
+
+var eightiesMovies = [movie1, movie2];
+
+var infant = eightiesMovies[1][4];
+
+console.log(infant);
+
+// TASK 6
+
+alert(eightiesMovies[0][0] + " " + eightiesMovies[0][1]);
+
+//OR
+
+alert(eightiesMovies[0].join(' '));
+
+
+
